985c8026991eb1e90151823ef10845c7c0cea03c
MohamedMohamed56 <mohamedm14@csp.edu>
2017-08-17 10:31:27 -0500

    Add files via upload

---
+++ Group Project for Advanced Calculator.cpp
@@ -0,0 +1,53 @@
+// Group Project for Advanced Calculator.cpp : Defines the entry point for the console application.
+//
+
+
+/*
+Group Project: Advanced Calculator
+
+Class: CSC 301 Programming for Problem Solving
+
+Written By: Mohamed Mohamed on August 14, 2017
+
+revised: August 15, 2017
+
+My Part of the Project: Taking user input and output of Mathematical Operators : Addition (+), Subtraction (-), Multiplication (*) and Division (/)
+
+*/
+
+
+#include "stdafx.h"
+
+#include <iostream> 
+
+#include <cmath>
+
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+	string mathOperator;
+	
+	cout << " Enter in your mathematical operator  + , -, *, / :  " << endl; // This is the user output for any mathematical operator they choose.
+
+	cin >> mathOperator; // User input for a math operator
+
+	cout << " Enter in your mathematical operator  +, -, *, / :  " <<endl; // This is the user output for any mathematical opertor they choose.
+
+	cin >> mathOperator; // User input for a math operator
+
+	cout << " Enter in your mathematical operator  +, -, *, / :  "  <<endl; // This is the user output for any mathematical operator they choose.
+
+	cin >> mathOperator; // User input for a math operator
+
+	cout << " Enter in your mathematical operator  +, -, *, /  : " <<endl; // This is the user output for any mathematical operator they choose.
+
+	cin >> mathOperator; // User input for a math operator
+
+    return 0; 
+}
+
